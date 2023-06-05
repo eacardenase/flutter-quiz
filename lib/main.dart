5c0ef9289d1c48b41b4e54a26460f7f2db790a59
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-05 16:02:57 -0500

    feat: set status bar color to white

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_quiz/quiz.dart';
 
 void main() {
   runApp(const Quiz());
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor:
+          Colors.white, // does not affect status bar color
+      statusBarColor: Colors.white, // does not affect status bar color
+      statusBarBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
 }
