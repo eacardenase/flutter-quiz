a7fb207198787de811e23e5939084c811f6c8ee0
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-05 18:53:22 -0500

    feat: finish styling layout

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary({
@@ -7,6 +8,9 @@ class QuestionsSummary extends StatelessWidget {
   });
 
   final List<Map<String, String>> summaryData;
+
+  final Color correctAnswerColor = Colors.cyanAccent;
+  final Color userAnswerColor = Colors.pinkAccent;
 
   @override
   Widget build(BuildContext context) {
@@ -18,23 +22,63 @@ class QuestionsSummary extends StatelessWidget {
             (summary) {
               return Row(
                 children: [
-                  Text(
-                    summary['question_index']!,
+                  Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(
+                          30,
+                        ),
+                      ),
+                      color: summary['user_answer'] == summary['correct_answer']
+                          ? correctAnswerColor
+                          : userAnswerColor,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          summary['question_index']!,
+                          style: GoogleFonts.lato(
+                            color: Colors.deepPurple,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
                   ),
                   Expanded(
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           summary['question']!,
+                          style: GoogleFonts.lato(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                          textAlign: TextAlign.start,
                         ),
                         const SizedBox(
                           height: 5,
                         ),
                         Text(
                           summary['user_answer']!,
+                          style: GoogleFonts.lato(
+                            color: userAnswerColor,
+                          ),
                         ),
                         Text(
                           summary['correct_answer']!,
+                          style: GoogleFonts.lato(
+                            color: correctAnswerColor,
+                          ),
                         ),
                       ],
                     ),
