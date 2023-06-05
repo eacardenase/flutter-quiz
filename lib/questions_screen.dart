c39465c22d795c59bd4744c25bd1506514b35357
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-05 10:41:09 -0500

    feat: add margins to questions screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -19,29 +19,35 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   Widget build(context) {
     final currentQuestion = questions[0];
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          currentQuestion.text,
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
+    return Container(
+      margin: const EdgeInsets.all(
+        40,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            currentQuestion.text,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        ...currentQuestion.answers.map(
-          (answer) {
-            return AnswerButton(
-              answerText: answer,
-              onTap: () {},
-            );
-          },
-        ),
-      ],
+          const SizedBox(
+            height: 30,
+          ),
+          ...currentQuestion.answers.map(
+            (answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {},
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
